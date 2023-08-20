555ada7fa8b9f70ae0d4db836e99dbed980f68fb
Janne Kivilahti <poppijanne@gmail.com>
2023-08-20 22:00:33 +0300

    base fixes

--- demo2/src/resource-loader.js
+++ demo2/src/resource-loader.js
@@ -1,32 +1,32 @@
 class ResourceLoader {
   loadTexture(gl, library, file) {
-    return loadTexture(gl, `/img/${library}/${file}`);
+    return loadTexture(gl, `./img/${library}/${file}`);
   }
   loadCubeTexture(gl, library) {
     return loadCubeTexture(gl, [
       {
         target: gl.TEXTURE_CUBE_MAP_POSITIVE_X,
-        url: `/img/${library}/pos-x.png`,
+        url: `./img/${library}/pos-x.png`,
       },
       {
         target: gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
-        url: `/img/${library}/neg-x.png`,
+        url: `./img/${library}/neg-x.png`,
       },
       {
         target: gl.TEXTURE_CUBE_MAP_POSITIVE_Y,
-        url: `/img/${library}/pos-y.png`,
+        url: `./img/${library}/pos-y.png`,
       },
       {
         target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Y,
-        url: `/img/${library}/neg-y.png`,
+        url: `./img/${library}/neg-y.png`,
       },
       {
         target: gl.TEXTURE_CUBE_MAP_POSITIVE_Z,
-        url: `/img/${library}/pos-z.png`,
+        url: `./img/${library}/pos-z.png`,
       },
       {
         target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Z,
-        url: `/img/${library}/neg-z.png`,
+        url: `./img/${library}/neg-z.png`,
       },
     ]);
   }
